4a44278c843f0773661951a213cd14df80ece80d
AlokKumarKamuni <alokkamuni@gmail.com>
2024-07-21 00:13:45 +0100

    commit7

--- app/src/main/java/uk/ac/tees/mad/w9642833/screens/MainScreen.kt
+++ app/src/main/java/uk/ac/tees/mad/w9642833/screens/MainScreen.kt
@@ -1,16 +1,27 @@
 package uk.ac.tees.mad.w9642833.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.Divider
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationDrawerItem
@@ -18,11 +29,19 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
+import coil.compose.rememberAsyncImagePainter
 import kotlinx.coroutines.launch
+import uk.ac.tees.mad.w9642833.models.CrimeReport
+import uk.ac.tees.mad.w9642833.viewmodel.HomeViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -54,7 +73,7 @@ fun MainScreen(navController: NavHostController) {
         }) { innerPadding ->
             Column(Modifier.padding(innerPadding)) {
 
-                Text(text = "Home Screen")
+                HomeScreen()
 
             }
         }
@@ -63,6 +82,20 @@ fun MainScreen(navController: NavHostController) {
 
 }
 
+@Composable
+fun HomeScreen(viewModel: HomeViewModel = viewModel()) {
+    val crimeReports by viewModel.crimeReports.collectAsState()
+
+    LazyColumn(
+        modifier = Modifier.fillMaxSize(),
+        contentPadding = PaddingValues(16.dp)
+    ) {
+
+        items(crimeReports) { report ->
+            Text(text = "${report.title}")
+        }
+    }
+}
 
 
 @Composable
